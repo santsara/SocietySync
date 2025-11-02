ce06aa421bea74981122c473cd47a6c0195dcc6f
sarasant <sant.sara.126@gmail.com>
2025-11-02 23:19:05 +0530

    Delete sosy-backend directory

--- sosy-backend/src/main/java/com/example/sosy/repository/FestivalBudgetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sosy.repository;
-
-import com.example.sosy.model.FestivalBudget;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FestivalBudgetRepository extends JpaRepository<FestivalBudget, Long> {
-}
